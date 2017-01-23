1eb466b95aa896e3fa278e6469534507dfc7070d
Frederick Solt <fsolt@mac.com>
2017-01-23 10:20:21 -0600

    initial commit

---
+++ R/load_cached_chunks.R
@@ -0,0 +1,17 @@
+setwd("paper")
+text <- readLines("campaign_participation.Rnw")
+chunks <- grep("^<<.*>>=", text, value = T)
+extractchunks <- gsub("^<<label=([^,>]*)[,>]*.*", "\\1", chunks)[-1]
+
+envir <- parent.frame()
+oldls <- ls(envir, all = TRUE)
+
+# For each chunk...
+for(ch in extractchunks){   
+  # Detect the file name of the database...
+  pat <- paste0("^", ch, ".*\\.rdb")
+  val <- gsub(".rdb", "", dir("cache", pattern = pat))
+  # Lazy load the database
+  lazyLoad(file.path("cache", val), envir = envir)
+}
+setwd("..")
